fcdaaded66f8cd1e7cbe86a47eea9deca35166f8
Joao Victor <victor.passon@yahoo.com.br>
2024-08-07 22:20:15 -0300

    PagContato

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,12 +21,14 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     /* Fetch Sobre */
-    document.querySelector(".sobre").addEventListener("click", () => carregar("../components/sobre.html", main));
-    document.querySelectorAll("footer div.fmenu ul li a")[0].addEventListener("click", () => carregar("../components/sobre.html", main));
+    document.querySelector(".sobre").addEventListener("click", () => carregar("https://raw.githubusercontent.com/victorpasson/-praTi-M1/atividade4/components/sobre.html", main));
+    document.querySelectorAll("footer div.fmenu ul li a")[0].addEventListener("click", () => carregar("https://raw.githubusercontent.com/victorpasson/-praTi-M1/atividade4/components/sobre.html", main));
 
     /* Fetch Contato */
-     document.querySelector(".contato").addEventListener("click", () => carregar("../components/contato.html", main));
-     document.querySelectorAll("footer div.fmenu ul li a")[1].addEventListener("click", () => carregar("../components/contato.html", main));
+     document.querySelector(".contato").addEventListener("click", async () => {
+        await carregar("https://raw.githubusercontent.com/victorpasson/-praTi-M1/atividade4/components/contato.html", main)
+    });
+     document.querySelectorAll("footer div.fmenu ul li a")[1].addEventListener("click", () => carregar("https://raw.githubusercontent.com/victorpasson/-praTi-M1/atividade4/components/contato.html", main));
 
     /* Página Inicio */
     document.querySelector("main article.newsletter form").addEventListener("submit", (event) => {
@@ -55,4 +57,47 @@ async function carregar (component, main){
             let doc = parse.parseFromString(html, "text/html").querySelector("body").innerHTML;
             main.innerHTML = doc;
         })
+        .then(() => processContato())
 }
+
+const processContato = () => {
+    const mensagem = document.querySelector("div.pcontato form span");
+
+    document.querySelector('div.pcontato form').addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const nome = document.querySelector("div.pcontato form input[name='name']");
+        const email = document.querySelector("div.pcontato form input[name='email']");
+        const assunto = document.querySelector("div.pcontato form input[name='assunto']");
+        const mensagemt = document.querySelector("div.pcontato form textarea");
+
+        if(nome.value.trim() == "" || email.value.trim() == "" || assunto.value.trim() == "" || mensagemt.value.trim() == "") {
+            mensagem.innerHTML = "Todos os campos devem ser preenchidos."
+            return 0;
+        }
+
+        const juntos = {
+            nome: nome.value,
+            email: email.value,
+            assunto: assunto.value,
+            mensagem: mensagemt.value
+        }
+
+
+        let arrayL = JSON.parse(window.localStorage.getItem("contato"));
+
+        if(arrayL) {
+            arrayL.push(juntos);
+            window.localStorage.setItem("contato", JSON.stringify(arrayL))
+        } else {
+            window.localStorage.setItem("contato", JSON.stringify([juntos]))
+        }
+
+        nome.value = "";
+        email.value = "";
+        assunto.value = "";
+        mensagemt.value = "";
+
+    })
+}
+
